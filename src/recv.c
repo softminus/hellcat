ecdbbb355a26d3c427583b805ae6e18e1f12c440
Thomas Mannay <audiobarrier@openmailbox.org>
2016-10-30 22:30:08 +0000

    Cleanup

--- src/recv.c
+++ src/recv.c
@@ -2,10 +2,9 @@
 
 /* the functions in this file handle the whole "receiving" thing */
 
-#include "net.h"
 #include <sys/time.h>
-
-
+#include <errno.h>
+#include "net.h"
 
 int main(int argc, char* argv[])
 {
@@ -17,24 +16,23 @@ int main(int argc, char* argv[])
     struct timeval start, end;
     float elapsed;
 
-    if ((argc != 5) && (argc != 4)) {
+    if (argc != 5 && argc != 4) {
         fprintf(stderr, "usage: %s chunksize dataport controlport [host]\nchunksize is in bytes\n", argv[0]);
         exit(10);
     }
 
-    if (argc == 4) {
+    if (argc == 4)
         listening = 1;
-    } else {
+    else
         listening = 0;
-    }
 
     bufsize = (size_t) atol(argv[1]);
     buf = malloc(bufsize);
     assert(buf != NULL);
 
-    if (listening == 0) {
+    if (listening == 0)
         controlfd = make_socket(argv[4], argv[3]);
-    } else {
+    else {
         control_listener = make_listener(argv[3]);
         data_listener = make_listener(argv[2]);
         controlfd = accept(control_listener, NULL, NULL);
@@ -49,8 +47,9 @@ int main(int argc, char* argv[])
 
     while (1) {
         rval = read(controlfd, &statusbyte, 1);
-
         if (rval != 1) {
+            if (errno == EINTR)
+                continue;
             perror("read on controlfd");
             exit(1);
         }
@@ -58,17 +57,18 @@ int main(int argc, char* argv[])
         if (statusbyte == DONE) {
             fprintf(stderr, "received DONE\n");
             gettimeofday(&end, NULL);
-            elapsed = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000;
-            fprintf(stderr, "transferred %zu bytes in %.3f seconds\n", totalsize, elapsed);
-
+            elapsed = (end.tv_sec - start.tv_sec) +
+                (end.tv_usec - start.tv_usec) / 1000000;
+            fprintf(stderr, "transferred %zu bytes in %.3f seconds\n",
+                totalsize, elapsed);
             break;
         }
 
-        assert (statusbyte == CONT);
+        assert(statusbyte == CONT);
 
-        if (listening == 0) {
+        if (listening == 0)
             datafd = make_socket(argv[4], argv[2]);
-        } else {
+        else {
             datafd = accept(data_listener, NULL, NULL);
             if (datafd == -1) {
                 perror("accept on data port");
@@ -77,19 +77,16 @@ int main(int argc, char* argv[])
         }
 
         rval = read_all(datafd, buf, bufsize);
-
         if (rval == -1) {
             perror("read on datafd");
             exit(1);
         }
-
         if (rval == 0) { /* eof on datafd */
             fprintf(stderr, "unexpected EOF on datafd\n");
             exit(1);
         }
 
         rval_send = write_all(1, buf, (size_t)rval);
-
         if (rval_send != rval) {
             perror("write on stdout");
             exit(1);
@@ -101,7 +98,6 @@ int main(int argc, char* argv[])
                 perror("write on controlfd");
                 exit(1);
             }
-
         }
 
         totalsize += (size_t) rval;
@@ -112,4 +108,3 @@ int main(int argc, char* argv[])
 
     return 0;
 }
-
